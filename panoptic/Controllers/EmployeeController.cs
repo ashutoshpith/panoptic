89a991d768e38ac2bd86a7f6f25699d827029617
ashutoshpith <amxdp18@gmail.com>
2019-03-04 02:25:33 +0530

    delete html and delete controller added

--- panoptic/Controllers/EmployeeController.cs
+++ panoptic/Controllers/EmployeeController.cs
@@ -84,5 +84,33 @@ namespace panoptic.Controllers
             }
             return View(employee);
         }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            Employee employee = objemployee.GetEmployeeData(id);
+
+            if(employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int? id)
+        {
+            objemployee.DeleteEmployee(id);
+            return RedirectToAction("Index");
+        }
+
     }
+
+
 }
