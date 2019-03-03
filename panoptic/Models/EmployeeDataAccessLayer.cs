b747c474c44433eb34989e5d3dbc41c3412107b1
ashutoshpith <amxdp18@gmail.com>
2019-03-03 23:30:23 +0530

    model view all emplyees

--- panoptic/Models/EmployeeDataAccessLayer.cs
+++ panoptic/Models/EmployeeDataAccessLayer.cs
@@ -3,13 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace panoptic.Models
 {
     public class EmployeeDataAccessLayer
     {
         string connectionString = "Data Source=ASHUTOSH;Integrated Security=True";
+        //to view all employees
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            List<Employee> lstemployee = new List<Employee>();
 
-        public IEnumerable<>
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                    employee.Name = rdr["Name"].ToString();
+                    employee.Gender = rdr["Gender"].ToString();
+                    employee.Department = rdr["Department"].ToString();
+                    employee.City = rdr["City"].ToString();
+
+                    lstemployee.Add(employee);
+                }
+                con.Close();
+
+            }
+            return lstemployee;
+        }
     }
 }
