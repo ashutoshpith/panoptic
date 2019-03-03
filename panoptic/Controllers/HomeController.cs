ed37996c2f39d35734f5da6d7e0c8794951fe16f
ashutoshpith <amxdp18@gmail.com>
2019-03-03 22:32:54 +0530

    employee model created

--- panoptic/Controllers/HomeController.cs
+++ panoptic/Controllers/HomeController.cs
@@ -12,6 +12,8 @@ namespace panoptic.Controllers
     {
         public IActionResult Index()
         {
+          
+
             return View();
         }
 
---
+++ panoptic/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace panoptic.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string connectionString = "Data Source=ASHUTOSH;Integrated Security=True";
+
+        public IEnumerable<>
+    }
+}
