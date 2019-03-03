345889cc84b590d4cc9146f2e940be9f1ccf70e7
ashutoshpith <amxdp18@gmail.com>
2019-03-04 01:24:27 +0530

    employee controller added

--- panoptic/Controllers/EmployeeController.cs
+++ panoptic/Controllers/EmployeeController.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using panoptic.Models;
 
 namespace panoptic.Controllers
 {
-    public class EmployeeController
+    public class EmployeeController : Controller
     {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        public IActionResult Index()
+        {
+            List<Employee> lstemployee = new List<Employee>();
+            lstemployee = objemployee.GetAllEmployees().ToList();
+
+            return View(lstemployee);
+        }
     }
 }
